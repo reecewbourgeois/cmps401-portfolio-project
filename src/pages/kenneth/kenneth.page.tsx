4988882b09608b57600e1cce4e9b8746c8990a72
Kennethcole5 <kenneth.cole@selu.edu>
2021-11-08 12:06:44 -0600

    Test first commit -Ken

--- src/pages/kenneth/kenneth.page.tsx
+++ src/pages/kenneth/kenneth.page.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 
+
+//Test commit - ken
+//
 export const KennethsPage: FC = () => {
-  return <h1>Kenneth's Page</h1>;
+  return <h1>Kenneth's Page</h1> <h2>Test</h2>;
 };
