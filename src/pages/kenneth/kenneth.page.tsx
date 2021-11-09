2c6ff04bf7fe3e601a5e845669137e21a481b757
Reece Bourgeois <reecebourgeois@gmail.com>
2021-11-09 10:31:57 -0600

    fix for Ken's page

--- src/pages/kenneth/kenneth.page.tsx
+++ src/pages/kenneth/kenneth.page.tsx
@@ -1,8 +1,12 @@
 import { FC } from 'react';
 
-
 //Test commit - ken
 //
 export const KennethsPage: FC = () => {
-  return <h1>Kenneth's Page</h1> <h2>Test</h2>;
+  return (
+    <div>
+      <h1>Kenneth's Page</h1>
+      <h2>Test</h2>
+    </div>
+  );
 };
