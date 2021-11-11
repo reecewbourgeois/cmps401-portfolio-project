5d019517bb9ee1fbe406f91d9612bffae4b11ddb
kwentin <kwentin00@gmail.com>
2021-11-11 09:28:35 -0600

    kwentin-push1

--- src/pages/kwentin/kwentin.page.tsx
+++ src/pages/kwentin/kwentin.page.tsx
@@ -1,5 +1,40 @@
-import { FC } from 'react';
+/**
+ * Notes:
+ *
+ * All JSX elements need to be in a return tag
+ * Must be one parent div
+ */
+
+import { FC } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./kwentin.css";
+import { Image } from "react-bootstrap";
+
+import kwenPortPic from "../../files/photos/kwentin/kwenPortPic.jpeg";
 
 export const KwentinsPage: FC = () => {
-  return <h1>Kwentin's Page</h1>;
-};
+  return (
+    <div className="parent">
+      <div className="row">
+        <div className="col">
+          <div className="left-side">
+            <div className="top-left-side">
+              {/*<Image className="kwen-img" src={kwenPortPic} />*/}
+              <p>this is a test</p>
+            </div>
+
+            <div className="bottom-left-side">
+              <p>this is a test</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col">
+          <div className="right-side">
+            <p>this is a test</p>
+          </div>
+        </div>
+      </div>
+    </div> //parent div
+  ); //return
+}; //KwentinsPage
