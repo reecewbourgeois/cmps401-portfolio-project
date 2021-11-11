ddd352e3745b0f60eeaa9bce89db5eca1dce86dc
Reece Bourgeois <reecebourgeois@gmail.com>
2021-11-11 12:44:07 -0600

    added blackjack game files

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -11,6 +11,7 @@ import pao from '../../files/photos/home/pao.jfif';
 export const Home: FC = () => {
   return (
     <div className="home-page-div">
+      {/* Title Section */}
       <h1>The Paogrammers</h1>
       <h3>Kenneth Cole, Kwentin Ransom, Reece Bourgeois, and Timothy McReynolds</h3>
       <hr />
@@ -108,9 +109,12 @@ export const Home: FC = () => {
         </Row>
       </Container>
       <hr />
+
+      {/* Pao's Section */}
       <div className="mb-3">
         <h1>
-          The <span className="man">Man</span>, The <span className="myth">Myth</span>, The <span className="legend">Legend</span> of the Computer Science Department
+          The <span className="man">Man</span>, The <span className="myth">Myth</span>, The{' '}
+          <span className="legend">Legend</span> of the Computer Science Department
         </h1>
         <Image src={pao} />
         <h1>Dr. Kuo-Pao "COBOL is the Best" Yang</h1>
@@ -118,6 +122,9 @@ export const Home: FC = () => {
         <h4>"I have no idea why we moved on from COBOL" - Pao</h4>
         <h4>"The Paogrammers deserve 100s" - Pao</h4>
       </div>
+      <hr />
+
+      {/* Blackjack Game */}
     </div>
   );
 };
