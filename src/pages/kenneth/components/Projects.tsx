2847f87a3beb035301ae66e85ad5b9253d37f607
Kennethcole5 <kenneth.cole@selu.edu>
2021-11-14 22:36:42 -0600

    Working on page pt2

--- src/pages/kenneth/components/Projects.tsx
+++ src/pages/kenneth/components/Projects.tsx
@@ -1,9 +1,9 @@
-import {FC} from "react";
-import {Grid, Card, CardMedia, CardContent, Typography} from "@mui/material"
+import { FC } from "react";
+import { Grid, Card, CardMedia, CardContent, Typography } from "@mui/material"
 import { CodeIcon } from "@heroicons/react/solid";
 
 import { projects } from "../data";
-import project1 from "../../../files/photos/kenneth/project1.jpg";
+import project2 from "../../../files/photos/kenneth/project2.jpg";
 
 export const Projects: FC = () => {
     return (
@@ -11,7 +11,7 @@ export const Projects: FC = () => {
             <div>
                 <div>
                     <Grid container spacing={2}>
-                        <Grid item xs={12} style={{display: 'flex'}}>
+                        <Grid item xs={12} style={{ display: 'flex' }}>
                             <div className="centered">
                                 <Typography variant="h3" className="kennethSectionHeader" >
                                     Apps I've Built
@@ -30,19 +30,19 @@ export const Projects: FC = () => {
                                                 <CardMedia
                                                     component="img"
                                                     height="140"
-                                                    image={project1}
+                                                    image={project2}
                                                     alt="green iguana"
                                                 />
                                                 <CardContent>
                                                     <Typography gutterBottom variant="h5" component="div">
-                                                    Lizard
+                                                        Full-Stack
                                                     </Typography>
                                                     <Typography variant="body2" color="text.secondary">
-                                                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                                                    species, ranging across all continents except Antarctica
+                                                        Full-Stack React and .NET app for Property Managers
                                                     </Typography>
                                                 </CardContent>
                                             </Card>
+
                                         </a>
                                     </div>
                                 ))}
--- src/pages/kenneth/data.ts
+++ src/pages/kenneth/data.ts
@@ -16,7 +16,7 @@ export const projects: KennethProject[] = [
         subtitle: "MERN Stack",
         description:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-1.jpg",
+        image: "./project2.jpg",
         link: "",
     },
     {
